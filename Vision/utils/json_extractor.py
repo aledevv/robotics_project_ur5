541a17b258c182e719d900ee6fa09cf5d2ac9db3
aledevv <alexsandev2@gmail.com>
2024-01-26 21:27:12 +0100

    world generation (to finish) and .world files

--- Vision/utils/json_extractor.py
+++ Vision/utils/json_extractor.py
@@ -248,8 +248,6 @@ def main():
     classes = [ 'X1-Y1-Z2', 'X1-Y2-Z1', 'X1-Y2-Z2', 'X1-Y2-Z2-CHAMFER', 'X1-Y2-Z2-TWINFILLET',
                 'X1-Y3-Z2', 'X1-Y3-Z2-FILLET', 'X1-Y4-Z1', 'X1-Y4-Z2', 'X2-Y2-Z2',
                 'X2-Y2-Z2-FILLET']
-    # TODO RICORDATI DI VERIFICARE CHE L'INDICE MESSO SIA GIUSTO (MAGARI QUI PARTE DA 0)
-    
     
     
     # let's get all data
