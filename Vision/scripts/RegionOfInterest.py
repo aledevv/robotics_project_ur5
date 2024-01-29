fe7963fca30b177a6cf4183500b17172cfb45da4
aledevv <alexsandev2@gmail.com>
2024-01-29 10:16:56 +0100

    finished ROI

--- Vision/scripts/RegionOfInterest.py
+++ Vision/scripts/RegionOfInterest.py
@@ -5,31 +5,29 @@ import sys
 import os
 
 # --------------------- DIRECTORIES and PATHS ---------------------
-ROOT = Path(__file__).resolve().parents[0]
+ROOT = Path(__file__).resolve().parents[1] # Vision folder
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
-ROOT = Path(os.path.relpath(ROOT, Path.cwd()))
+ROOT = Path(os.path.abspath(ROOT))
 
-INPUT_FILE = os.path.join(ROOT, '/log/img_ZED_cam.png')
-OUTPUT_DIR = os.path.join(ROOT, '/log/')
+INPUT_FILE = str(ROOT) + '/log/img_ZED_cam.png'
+OUTPUT_DIR = str(ROOT) + '/log/ROI_table.png'
 
-# --------------------- REGION OF INTEREST CLASS ---------------------
-
-
-class ROI:
-
-    def __int__(self):
-        self.img = cv2.imread(INPUT_FILE)
-
-    def find_roi(self):
-        mask = np.zeros(self.img.shape[0:2], dtype=np.uint8)
-        points = np.array([[[845, 409], [1201, 412], [1545, 913], [658, 921]]])
-        cv2.drawContours(mask, [points], -1, (255, 255, 255), -1, cv2.LINE_AA)
-        res = cv2.bitwise_and(self.img, self.img, mask=mask)
-        cv2.imwrite(OUTPUT_DIR, res)
-        cv2.destroyAllWindows()
+# --------------------- FUNCTIONS  ---------------------
 
 
+def find_roi(img):
+    mask = np.zeros(img.shape[0:2], dtype=np.uint8) # Mask has the same dimensions of zed cam image
+    points = np.array([[[844, 410], [1196, 410], [1541, 905], [678, 905]]]) # corners of the table
+    cv2.drawContours(mask, [points], -1, (255, 255, 255), -1, cv2.LINE_AA)
+    res = cv2.bitwise_and(img, img, mask=mask)
+    cv2.imwrite(OUTPUT_DIR, res)
+
+
+
+# --------------------- MAIN  ---------------------
 if __name__ == '__main__':
-    roi = ROI()
-    roi.find_roi()
+
+    image = cv2.imread(INPUT_FILE)
+
+    find_roi(image)
